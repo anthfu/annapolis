e112008b6adcda1bb700ad48e08fce7b53c43f4c
Anthony Hrvojic <1017633+anthfu@users.noreply.github.com>
2022-04-24 22:18:10 -0400

    Implement minimal Jetty server

--- src/annapolis/server.clj
+++ src/annapolis/server.clj
@@ -1,1 +1,15 @@
-(ns annapolis.server)
+(ns annapolis.server
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/api"
+       ["/ping" {:get (fn [_] {:status 200
+                               :body "ok"})}]])
+    (ring/create-default-handler)))
+
+(defn start []
+  (jetty/run-jetty #'app {:port 3000, :join? false})
+  (println "Server running on port 3000"))
